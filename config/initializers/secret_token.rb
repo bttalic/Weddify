8e22a1a7bab955948bdf5d4ad4920be8c1863b97
bttalic <b.ttalic@gmail.com>
2013-07-26 02:42:45 +0200

    First static version of Weddify the planner

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weddify::Application.config.secret_token = '489984832c9d2feb7771a50c636212cb398b18da06e85670b9eb342b556d44ffef73567523682fed6352c367e343623d0c1d7246df51896054d63809ac54dac3'
