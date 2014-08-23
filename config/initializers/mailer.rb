a8ba2fcfd1f6b15d6cdecb57dc9375a0eee7668b
bttalic <b.ttalic@gmail.com>
2014-08-23 23:11:00 +0200

    Delete mailer.rb

--- config/initializers/mailer.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => 'gmail.com',
-   :authentication => :plain,
-   :user_name=>"b.ttalic@gmail.com",
-   :password=>'talic22029020',
-   :enable_starttls_auto => true
-}
